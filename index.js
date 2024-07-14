d8fb9e4f24dde2aaebb8c7e123a23e6c33ebdb69
Andre-Vlt <andrelvalentim5@gmail.com>
2024-07-13 23:23:32 -0300

    implementacao de rotas utilizando prisma e MongoDB

--- index.js
+++ index.js
@@ -17,28 +17,19 @@ app.post('/order', async (req,res) => {
               value: req.body.valorTotal,  
               creationDate: req.body.dataCriacao,
               Items: {
-                create: {
-                  productId: req.body.idItem, 
-                  quantity: req.body.quantidadeItem,  
-                  price: req.body.valorItem
-                }
-              }
-            },
-            include: {
-              items: true 
+                create: req.body.items.map(item => ({
+                  productId: item.idItem,
+                  quantity: item.quantidadeItem,
+                  price: item.valorItem
+                }))
+              },
+            }, include: {
+                Items:true
             }
           });
 
 
-        // await prisma.order.create({
-        //     data:{
-        //         orderId: req.body.numeroPedido,
-        //         value: req.body.valorTotal,
-        //         creationDate: req.body.dataCriacao               
-        //     }
-        // })
-
-        res.send('OK')
+        res.status(201).send({novoPedido})
     }
     catch (erro) {
         console.error(erro.message);
@@ -48,40 +39,47 @@ app.post('/order', async (req,res) => {
 
 app.get('/order/list', async(req, res)=>{
     try{
-        const todosPedidos = await pool.query('SELECT * FROM Order');
-        res.json(todosPedidos.rows);
+       const orders = await prisma.order.findMany();
+       res.json(orders); 
     }
     catch (erro){
         console.error(erro.message);
-        res.status(500).send('Erro buscando pedidos');
+        res.status(404).send('Pedido não encontrado');
     }
 });
 
 app.get('/order/:orderId', async (req, res) => {
     try{
-    const {orderId} = req.params;
-    const pedido = await pool.query('SELECT *FROM Order WHERE id = $1', [orderId]);
-    if (pedido.rows.length === 0){
-        return res.status(404).send('Pedido não encontrado');
-    }
-    res.json(pedido.rows[0]);
+        const {orderId} = req.params;
+        const order = await prisma.order.findUnique({
+            where: {orderId: orderId},
+            include:{Items:{} }
+        });
+        res.status(200).json(order);
 }
 catch(erro){
     console.error(erro.message);
-    res.status(500).send('Erro ao buscar pedido');
+    res.status(404).send('Pedido não encontrado');
 }
 });
 
 app.put('/order/:orderId', async (req, res)=> {
+    const {orderId} = req.params;
+    const {value, itens, creationDate} = req.body;
     try{
-        const {orderId} = req.params;
-        const {quantity, price} = req.body;
-        const updateOrder = await pool.query('UPDATE Items SET quantity = $1, price = $2 WHERE orderID = $3', [quantity, price, orderId]);
-        if (updateOrder.rows.length === 0){
-            return res.status(404).send('Pedido não encontrado');
-        }
 
-        res.json(updateOrder.rows[0]);
+        const UpdatedOrder = await prisma.order.update({
+           where: {
+            orderId: orderId
+        },
+           data:{
+            value: value,
+            creationDate: creationDate
+           } 
+        });
+        
+        res.json({pedido: UpdatedOrder});
+        
         
     }catch(erro){
         console.error(erro.message);
@@ -93,13 +91,13 @@ app.put('/order/:orderId', async (req, res)=> {
 app.delete('/order/:orderId', async (req, res) => {
     try{
         const {orderId} = req.params;
-        const deleteOrder = await pool.query('DELETE FROM Order where orderId = $1 RETURNING *', [orderId]);
+        await prisma.order.delete({
+            where: {
+                orderId: orderId
+            }
+        })
 
-        if(deleteOrder.rows.length === 0){
-            return res.status(404).send('Pedido não encontrado');
-        }
-
-        res.json(deleteOrder.rows[0]);
+        res.json("Pedido deletado");
     }catch(erro){
         console.error(erro.message);
         res.status(500).send('Erro ao deletar pedido');
